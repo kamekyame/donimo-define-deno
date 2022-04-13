cadf6a3b0bc253f07de920fd54d31d1dffffb8cd
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-04-14 01:35:47 +0900

    feat: 使用しているIDのリストを出力するように変更 (#7)
    * feat: 使用しているIDのリスを出力するように変更
    
    * fix: CCMFolder テストのIDを追加
    
    * test: Duplicate IDを追加

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
+export * as log from "https://deno.land/std@0.134.0/log/mod.ts";
+
 export * from "https://deno.land/x/xml@2.0.4/mod.ts";
 export * from "https://deno.land/x/xml@2.0.4/utils/types.ts";
--- test/CCMFolder.test.ts
+++ test/CCMFolder.test.ts
@@ -6,6 +6,20 @@ Deno.test({
   fn: () => {
     const fileStr = `<?xml version="1.0" encoding="Shift_JIS"?>
     <ModuleData Name="test"><ControlChangeMacroList>
+    <Folder ID="1"></Folder>
+    <Folder ID="1"></Folder>
+  </ControlChangeMacroList></ModuleData>`;
+    assertThrows(() => {
+      Domino.File.fromXML(fileStr);
+    }, Domino.DominoError);
+  },
+});
+
+Deno.test({
+  name: "[CCMFolder] Invalid Name",
+  fn: () => {
+    const fileStr = `<?xml version="1.0" encoding="Shift_JIS"?>
+    <ModuleData Name="test"><ControlChangeMacroList>
     <Folder Dummy="aaa">
     </Folder>
   </ControlChangeMacroList></ModuleData>`;
