01d1edd4132770ae0972ec0b8493a7e029a29694
Suzu Tomo <kamekyame@outlook.com>
2022-05-05 21:43:53 +0900

    fix: Fix test

--- deps.test.ts
+++ deps.test.ts
@@ -1,1 +1,3 @@
 export * from "https://deno.land/std@0.122.0/testing/asserts.ts";
+
+export { default as Encoding } from "https://esm.sh/encoding-japanese";
--- test/restore-file.test.ts
+++ test/restore-file.test.ts
@@ -1,11 +1,13 @@
 import * as Domino from "../mod.ts";
+import { Encoding } from "../deps.test.ts";
 
 const folderPath = "./test/sample-files";
 
 for await (const dirEntry of Deno.readDir(folderPath)) {
   if (!dirEntry.isFile) continue;
   // console.log(dirEntry);
-  const str = await Deno.readTextFile(`${folderPath}/${dirEntry.name}`);
+  const f = await Deno.readFile(`${folderPath}/${dirEntry.name}`);
+  const str = Encoding.convert(f, { to: "UTF8", from: "SJIS", type: "string" });
 
   Deno.test({
     name: `[restore-file] ${dirEntry.name}`,
