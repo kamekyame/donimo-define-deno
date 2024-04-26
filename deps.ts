a9a44d9b523d5981313787988b788f881f3da9fc
kamekyame <kamekyame@users.noreply.github.com>
2024-04-26 00:42:53 +0000

    build(deps): Bump deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Copyright 2022 kamekyame. All rights reserved. MIT license.
 
-export { EOL, format } from "https://deno.land/std@0.134.0/fs/mod.ts";
-export * as log from "https://deno.land/std@0.134.0/log/mod.ts";
+export { EOL, format } from "https://deno.land/std@0.224.0/fs/mod.ts";
+export * as log from "https://deno.land/std@0.224.0/log/mod.ts";
 
 export { xml2js } from "https://deno.land/x/xml2js@1.0.0/mod.ts";
 export {
